fb412c67509d76688830abc575bdf6e412016bd3
aboammar <aboomaraboammar@gmail.com>
2023-03-11 19:35:09 +0200

    dates reminder firest

---
+++ src/data.js
@@ -0,0 +1,7 @@
+export const person = [{ id: '0', name: 'حسن', date: '5 مساء', img: 'ff.jpg' },
+{ id: '1', name: 'محمد', date: '4 مساء', img: 'ff.jpg' },
+{ id: '2', name: 'على', date: '3 مساء', img: 'ff.jpg' },
+{ id: '3', name: 'محمود', date: '2 مساء', img: 'ff.jpg' },
+{ id: '4', name: 'حسن', date: '1 مساء', img: 'ff.jpg' },
+{ id: '5', name: 'حسن', date: '1 مساء', img: 'ff.jpg' },
+]
